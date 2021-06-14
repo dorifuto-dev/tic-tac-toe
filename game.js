080679822d9e52bb120aeffafc524df220c444c7
Eric Li <ericli@knights.ucf.edu>
2021-06-14 17:24:35 -0600

    Refactor Game class

--- game.js
+++ game.js
@@ -2,8 +2,21 @@ class Game {
   constructor() {
     this.currentPlayer = "🦕";
     this.winner = null;
-    this.board = null;
-    this.winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8], [1, 4, 7], [0, 3, 6], [0, 4, 8], [2, 4, 6]];
-    this.id = Date.now();
+    this.turn = null;
+    this.board = ["", "", "", "", "", "", "", "", ""];
+    this.winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 5, 8],
+      [1, 4, 7],
+      [0, 3, 6],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  }
+
+  clearBoard() {
+    this.board = ["", "", "", "", "", "", "", "", ""];
   }
 }
