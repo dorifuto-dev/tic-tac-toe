c81d7479dd6c2d1ea711cd84c43dd47a120a571e
Eric Li <ericli@knights.ucf.edu>
2021-06-15 11:30:33 -0600

    Add function changeTurn

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // Global Variables
-var currentToken = `./assets/dino.svg`;
+var currentToken = `./assets/meteor.svg`;
+var player1 = new Player("🦕");
+var player2 = new Player("☄️");
 var testGame = new Game();
+testGame.players.push(player1);
+testGame.players.push(player2);
 
 // Query Selectors
 var gameBoard = document.querySelector(".tic-tac-toe");
+var whosTurn = document.querySelector("h1");
 
 // Event Listeners
 gameBoard.addEventListener("click", function(event) {
@@ -14,29 +19,36 @@ function checkWinner(index) {
   for (var i = 0; i < testGame.winningCombos.length; i++) {
     if (testGame.winningCombos[i].every(elem => testGame.board[index].includes(elem))) {
       console.log("Winner!!!");
+      testGame.players[index].wins++;
+      console.log(testGame.players[index].wins);
       return true;
     }
   }
   return false;
 }
 
-
+function changeTurn() {
+  if (currentToken === `./assets/dino.svg`) {
+    whosTurn.innerText = "It's ☄️'s turn";
+    currentToken = `./assets/meteor.svg`;
+  } else if (currentToken === `./assets/meteor.svg`) {
+    whosTurn.innerText = "It's 🦕's turn";
+    currentToken = `./assets/dino.svg`;
+  }
+}
 
 function placeToken(event) {
   console.log(event.target.id);
   var tag = parseInt(event.target.id);
+  changeTurn();
   if (event.target.closest("div").innerHTML === "" && currentToken === `./assets/dino.svg`) {
     event.target.closest("div").innerHTML += `<img src="${currentToken}" class="dino-toe">`;
-    currentToken = `./assets/meteor.svg`;
     testGame.board[0].splice(tag, 1, tag);
-    console.log(testGame.board[0]);
     checkWinner(0);
 
   } else if (event.target.closest("div").innerHTML === "" && currentToken === `./assets/meteor.svg`) {
     event.target.closest("div").innerHTML += `<img src="${currentToken}" class="dino-toe">`;
-    currentToken = `./assets/dino.svg`;
     testGame.board[1].splice(tag, 1, tag);
-    console.log(testGame.board[1]);
     checkWinner(1);
   }
 }
