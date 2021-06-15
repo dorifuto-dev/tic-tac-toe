b64062a8c8788bfaa29a84a92e77285d1e660e2f
Eric Li <ericli@knights.ucf.edu>
2021-06-14 19:08:30 -0600

    Allow user to alternate between tokens

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ gameBoard.addEventListener("click", function(event) {
 // Event Handlers
 function placeToken(event) {
   console.log(event.target.id);
-  if (event.target.closest("div").innerHTML === "") {
+  if (event.target.closest("div").innerHTML === "" && currentToken === `./assets/dino.svg`) {
     event.target.closest("div").innerHTML += `<img src="${currentToken}" class="dino-toe">`;
-  }    
+    currentToken = `./assets/meteor.svg`;
+  } else if (event.target.closest("div").innerHTML === "" && currentToken === `./assets/meteor.svg`) {
+    event.target.closest("div").innerHTML += `<img src="${currentToken}" class="dino-toe">`;
+    currentToken = `./assets/dino.svg`;
+  }
 }
